01e8aba53c96072ebe1cbbe4d92025de633c4c14
anuragawasthi13 <anuragawasthi13@gmail.com>
2016-11-07 19:46:36 +0530

    updated twitter callback url

--- app/passport/configAuth.js
+++ app/passport/configAuth.js
@@ -2,6 +2,6 @@ module.exports = {
 	"twitterAuth": {
 		consumerKey:"IizN1Lt9Yg11k18GOvJPNq4Ej",
 		consumerSecret:"EtS76vPoLr2PrMlLva3gHd2pEdGyXXuMqvOJLFdnBDHKf1k9d4",
-		callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+		callbackURL: "https://morning-shelf-79437.herokuapp.com/auth/twitter/callback"
 	}
 }
