5b2c33748222f4043765801c334274eca79460c4
anuragawasthi13 <anuragawasthi13@gmail.com>
2016-11-07 19:40:28 +0530

    added configAuth file

---
+++ app/passport/configAuth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"twitterAuth": {
+		consumerKey:"IizN1Lt9Yg11k18GOvJPNq4Ej",
+		consumerSecret:"EtS76vPoLr2PrMlLva3gHd2pEdGyXXuMqvOJLFdnBDHKf1k9d4",
+		callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+	}
+}
